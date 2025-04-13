fcd3664cc02ddd0a358fc608d183baf26adc62ed
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-04-13 10:15:45 +1000

    delete resume section

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -47,14 +47,6 @@ const Sidenav = () => {
             <span className="pl-4">Projects</span>
           </a>
           <a
-            href="/resume.pdf"
-            target="_blank"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <BsPerson size={20} />
-            <span className="pl-4">Resume</span>
-          </a>
-          <a
             onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
@@ -85,13 +77,6 @@ const Sidenav = () => {
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineProject size={20} />
-          </a>
-          <a
-            href="/resume.pdf"
-            target="_blank"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <BsPerson size={20} />
           </a>
           <a
             href="#contact"
